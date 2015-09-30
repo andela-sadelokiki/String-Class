96a173de9b212774a5c3805d890a3d55a19ce98c
Susan <susan.adelokiki@andela.co>
2015-09-30 15:42:01 +0100

    Refactor comments, toUpper and toLower methods.

--- jasmine/src/stringClass.js
+++ jasmine/src/stringClass.js
@@ -1,63 +1,56 @@
 'use strict';
 
-var result, check;
+var check = null;
 
+//checks if string contains vowel and returns corresponding boolean value
 String.prototype.hasVowels = function() {
-
-  //check if string contains vowel and returns corresponding boolean value
   check = /[aeiou]+/i;
-  result = check.test(this);
-  return result;
+  return check.test(this);
 };
 
+//converts string to uppercase by performing bitwise AND operation on the ASCII alphabetic characters
 String.prototype.toUpper = function() {
-  result = this.toUpperCase();
-  return result;
+  return this.replace(/[a-z]/g, function(txt) {
+    return String.fromCharCode(txt.charCodeAt(0) & ~32)
+  });
 };
 
+//converts string to lowercase by performing bitwise AND operation on the ASCII alphabetic characters
 String.prototype.toLower = function() {
-  result = this.toLowerCase();
-  return result;
+  return this.replace(/[A-Z]/g, function(txt) {
+    return String.fromCharCode(txt.charCodeAt(0) + 32)
+  });
 };
 
+//converts first character to uppercase and other characters to lower case
 String.prototype.ucFirst = function() {
-
-  //converts first character to uppercase and other characters to lower case
-  result = this.replace(/\w\S*/g, function(txt) {
+  return this.replace(/\w\S*/g, function(txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
-  return result;
 };
 
+//checks if string is a question and returns corresponding boolean value
 String.prototype.isQuestion = function() {
-
-  //check is string is a question and returns corresponding boolean value
   check = /[^?]+\?/;
-  result = check.test(this);
-  return result;
+  return check.test(this);
 };
 
+//returns list of words in string as an array
 String.prototype.words = function() {
-
-  //returns list of words in string as an array
-  result = this.split(/\s+/);
-  return result;
+  return this.split(/\s+/);
 };
 
+//returns the number of words in the string
 String.prototype.wordCount = function() {
   return this.words().length;
 };
 
+//three digits sets are matched with comma and the remaining digit(s)(capture group 1)from the end of the string
 String.prototype.toCurrency = function() {
-
-  //three digits sets are matched with comma and the remaining digit(s)(capture group 1)from the end of the string
-  result = this.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-  return result;
+  return this.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 };
 
+//digit followed by decimal is replaced with empty string and a number is returned
 String.prototype.fromCurrency = function() {
-
-  //digit followed by decimal is replaced with empty string and a number is returned
-  result = parseFloat(this.replace(/[^\d\.]/g, ''));
-  return result;
+  return parseFloat(this.replace(/[^\d\.]/g, ''));
 };
